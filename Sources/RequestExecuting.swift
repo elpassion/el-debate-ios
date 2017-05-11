f40748903101cf9fb9590ab901f84900478b7138
pawurb <p.urbanek89@gmail.com>
2017-05-11 11:29:56 +0200

    initial request executor implementation

--- Sources/RequestExecuting.swift
+++ Sources/RequestExecuting.swift
@@ -10,3 +10,19 @@ protocol RequestExecuting {
     func post(url: URL, body: Parameters?) -> JSONResponseProviding
 
 }
+
+extension DataRequest: JSONResponseProviding {
+
+    func json(completionHandler: @escaping (Any?) -> Void) {
+        responseJSON { dataRequest in
+            completionHandler(dataRequest.value)
+        }
+    }
+}
+
+class RequestExecutor: RequestExecuting {
+
+    func post(url: URL, body: Parameters?) -> JSONResponseProviding {
+        return Alamofire.request(url, method: .post, parameters: body, encoding: JSONEncoding.default).validate()
+    }
+}
