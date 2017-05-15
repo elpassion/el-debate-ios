d61c339eb36802bd5e0f369d5dd6c9c6cba28acb
pawurb <p.urbanek89@gmail.com>
2017-05-15 09:48:16 +0200

    Auth token keychain storage and specs

---
+++ Tests/Mocks/KeychainMock.swift
@@ -0,0 +1,23 @@
+//
+//  KeychainMock.swift
+//  ELDebate
+//
+//  Created by Pawel Urbanek on 15/05/2017.
+//  Copyright © 2017 EL Passion. All rights reserved.
+//
+
+class KeychainMock: KeychainStoring {
+    var storedValues: [String: Any?] = [:]
+
+    func set(_ value: String, key: String) throws {
+        storedValues[key] = value
+    }
+
+    func get(_ key: String) throws -> String? {
+        return storedValues[key] as? String
+    }
+
+    func remove(_ key: String) throws {
+        storedValues[key] = nil
+    }
+}
