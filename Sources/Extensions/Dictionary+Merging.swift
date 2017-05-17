8b10b9e4584f3d8015dcee1df0425ce8bb47e2e1
Jakub Turek <jkbturek@gmail.com>
2017-05-17 15:57:53 +0200

    Implemented mapping values and merging dicts

---
+++ Sources/Extensions/Dictionary+Merging.swift
@@ -0,0 +1,24 @@
+//
+//  Created by Jakub Turek on 17.05.2017.
+//  Copyright © 2017 EL Passion. All rights reserved.
+//
+
+import Foundation
+
+extension Dictionary {
+
+    func merge(_ otherDictionary: [Key: Value]) -> [Key: Value] {
+        var result: [Key: Value] = [:]
+
+        for (key, value) in self {
+            result[key] = value
+        }
+
+        for (key, value) in otherDictionary {
+            result[key] = value
+        }
+
+        return result
+    }
+
+}
