1575acbad0aae0d03efc1fa65cee4c069edc00e1
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-09-19 14:47:28 +0200

    Fix tests for Extension.

--- Tests/Extensions/DictionaryNSAttributedStringKeySpec.swift
+++ Tests/Extensions/DictionaryNSAttributedStringKeySpec.swift
@@ -7,13 +7,14 @@ class DictionaryNSAttributedStringKeySpec: QuickSpec {
     override func spec() {
         describe("Dictionary") {
             it("should convert type of value from NSAttributedStringKey to String") {
-                let dictionary: [NSAttributedStringKey: NSObject] = [
+                let dictionary: [NSAttributedStringKey: AnyObject] = [
                     NSAttributedStringKey.font: UIFont.systemFont(ofSize: 18)]
 
-                let expectation: [String: Any] = [
-                    NSAttributedStringKey.font.rawValue: UIFont.systemFont(ofSize: 18)]
+                let expectedDictionary: [String: Any] = [
+                    NSAttributedStringKey.font.rawValue: UIFont.systemFont(ofSize: 18)
+                ]
 
-                expectation(dictionary.stringAny.description) == expectation(description: description)
+                expect(dictionary.stringAny.description) == expectedDictionary.description
             }
         }
     }
