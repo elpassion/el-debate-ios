5f646e1367c3ac054f17c0e3dcd758787c80b384
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-09-25 09:40:38 +0200

    Remove another swiftlint disabling comment.

--- Sources/Framework/Models/VoteContext.swift
+++ Sources/Framework/Models/VoteContext.swift
@@ -8,11 +8,9 @@ public struct VoteContext {
 
 }
 
-// swiftlint:disable no_extension_access_modifier
-
 public extension VoteContext {
 
-    func answer(for answerType: AnswerType) -> Answer {
+    public func answer(for answerType: AnswerType) -> Answer {
         switch answerType {
         case .positive:
             return debate.positiveAnswer
