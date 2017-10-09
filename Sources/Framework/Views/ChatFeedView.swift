31a6534c253c65e742ff30f359efc781c44d9a6b
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-10-09 15:34:51 +0200

    Configure Chatfeed View.

--- Sources/Framework/Views/ChatFeedView.swift
+++ Sources/Framework/Views/ChatFeedView.swift
@@ -1,1 +1,36 @@
-import Foundation
+import Anchorage
+import UIKit
+
+class ChatFeedView: UIView {
+
+        private let background: UIImageView = Views.image(image: .loginBackground, contentMode: .scaleAspectFit)
+
+    public init() {
+        super.init(frame: .zero)
+
+        setUpSubviews()
+        addSubviews()
+        setUpLayout()
+    }
+
+    private func setUpSubviews() {
+        backgroundColor = UIColor(predefined: .screenBackground)
+    }
+
+    private func addSubviews() {
+        addSubview(background)
+    }
+
+    private func setUpLayout() {
+        background.centerXAnchor == centerXAnchor
+        background.bottomAnchor == bottomAnchor - 20
+        background.widthAnchor == widthAnchor * 0.95
+        background.heightAnchor == background.widthAnchor * 0.75
+    }
+
+    // MARK: - Required initializer
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
