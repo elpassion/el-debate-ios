915a9ec3c474a56221ed0942ed6ddca2ee433333
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-10-14 09:20:59 +0200

    Extract FetchDebateService from APIClient class.

--- Sources/Framework/Networking/Services/FetchDebateService.swift
+++ Sources/Framework/Networking/Services/FetchDebateService.swift
@@ -4,8 +4,31 @@ import PromiseKit
 
 protocol FetchDebateServiceProtocol {
 
+    func fetchDebate(authToken: String) -> Promise<Debate>
+
 }
 
 class FetchDebateService: FetchDebateServiceProtocol {
+
+    private let requestExecutor: RequestExecuting
+    private let debateDeserializer: Deserializer<Debate>
+    private let URLProvider: URLProviding
+
+    init(requestExecutor: RequestExecuting,
+         debateDeserializer: Deserializer<Debate>,
+         URLProvider: URLProviding) {
+        self.requestExecutor = requestExecutor
+        self.debateDeserializer = debateDeserializer
+        self.URLProvider = URLProvider
+    }
+
+    func fetchDebate(authToken: String) -> Promise<Debate> {
+        let jsonResponse = requestExecutor.get(
+            url: "\(URLProvider.url)/api/debate",
+            headers: ["Authorization": authToken]
+        )
+
+        return request(with: jsonResponse.json, deserializedBy: self.debateDeserializer)
+    }
 
 }
