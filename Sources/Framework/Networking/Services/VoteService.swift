9c4f19e1b645b48514db9a86fa3edb3dc91bc0d3
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-10-14 09:13:59 +0200

    Extract VoteService from APIService class.

--- Sources/Framework/Networking/Services/VoteService.swift
+++ Sources/Framework/Networking/Services/VoteService.swift
@@ -4,8 +4,37 @@ import PromiseKit
 
 protocol VoteServiceProtocol {
 
+    func vote(authToken: String, answer: Answer) -> Promise<Answer>
+
 }
 
 class VoteService: VoteServiceProtocol {
+
+    private let requestExecutor: RequestExecuting
+    private let URLProvider: URLProviding
+
+    init(requestExecutor: RequestExecuting,
+         URLProvider: URLProviding) {
+        self.requestExecutor = requestExecutor
+        self.URLProvider = URLProvider
+    }
+
+    func vote(authToken: String, answer: Answer) -> Promise<Answer> {
+        let response = requestExecutor.post(
+            url: "\(URLProvider.url)/api/vote",
+            body: ["id": answer.identifier],
+            headers: ["Authorization": authToken]
+        )
+
+        return Promise(requestExecutor: response.maybeJson) { _ in answer }
+            .recover { error -> Promise<Answer> in
+                if case let AFError.responseValidationFailed(reason: .unacceptableStatusCode(code: code)) = error,
+                    code == 429 {
+                    return Promise(error: RequestError.throttling)
+                } else {
+                    return Promise(error: error)
+                }
+            }
+    }
 
 }
