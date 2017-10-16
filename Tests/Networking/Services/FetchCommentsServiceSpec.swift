3a612986310c0f869dfc79959588568a317ac6cb
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-10-16 16:49:50 +0200

    Rename CommentsJSONMocks, add tests for closed/open debate.

--- Tests/Networking/Services/FetchCommentsServiceSpec.swift
+++ Tests/Networking/Services/FetchCommentsServiceSpec.swift
@@ -25,6 +25,34 @@ class FetchCommentsServiceSpec: QuickSpec {
                 fetchCommentsService = nil
             }
 
+            describe("fetchDebate") {
+                context("debate is NOT closed") {
+                    beforeEach {
+                        requestExecutor.getReturnValue = FetchCommentsJSONMock()
+                    }
+
+                    it("returns deserialized comments for NOT closed debate") {
+                        var comments: Comments?
+                        _ = fetchCommentsService.fetchComments(authToken: "").then { commentsResult in
+                            comments = commentsResult
+                        }
+                        expect(comments?.debateClosed).toEventually(equal(false))
+                    }
+                }
+                context("debate is closed") {
+                    beforeEach {
+                        requestExecutor.getReturnValue = DebateClosedFetchCommentsJSONMock()
+                    }
+
+                    it("returns deserialized comments for closed debate") {
+                        var comments: Comments?
+                        _ = fetchCommentsService.fetchComments(authToken: "").then { commentsResult in
+                            comments = commentsResult
+                        }
+                        expect(comments?.debateClosed).toEventually(equal(true))
+                    }
+                }
+            }
         }
     }
 }
