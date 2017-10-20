6e9379610d7e0dbc78cb98019d94ab809f86cc1d
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-10-20 23:46:30 +0200

    Change SingleCommetnView

--- Sources/Framework/Views/SingleCommentView.swift
+++ Sources/Framework/Views/SingleCommentView.swift
@@ -1,5 +1,32 @@
 import Anchorage
+import Foundation
+import UIColor_Hex_Swift
 import UIKit
 
-class SingleCommentView: UITableViewCell {
+class SingleCommentView: UIView {
+
+    public let userInitialsLabel: UILabel = UILabel()
+
+    init() {
+        super.init(frame: .zero)
+
+        setupSubviews()
+        addLayoutSubviews()
+    }
+
+    private func setupSubviews() {
+
+    }
+
+    private func addLayoutSubviews() {
+        addSubview(userInitialsLabel)
+        userInitialsLabel.centerAnchors == centerAnchors
+    }
+
+    // MARK: - Required initializer
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
 }
