9c9120866f57c7720ccb035314318ef23792b054
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-10-23 10:49:28 +0200

    Add new properties for Custom Cell View.

--- Sources/Framework/Views/SingleCommentView.swift
+++ Sources/Framework/Views/SingleCommentView.swift
@@ -5,7 +5,11 @@ import UIKit
 
 class SingleCommentView: UIView {
 
+    public let userNameLabel: UILabel = UILabel()
+    public let userAvatarView: UIView = UIView(frame: .zero)
     public let userInitialsLabel: UILabel = UILabel()
+    public let dateLabel: UILabel = UILabel()
+    public let commentContextLabel: UILabel = UILabel()
 
     init() {
         super.init(frame: .zero)
