c486a91d96db96f16d5b3ec2efb782e100811069
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-10-26 16:54:27 +0200

    Add CommentsServicesAssembly

--- Sources/Framework/Dependency Injection/APIAssembly.swift
+++ Sources/Framework/Dependency Injection/APIAssembly.swift
@@ -5,19 +5,6 @@ import SwinjectAutoregistration
 class APIAssembly: Assembly {
 
     func assemble(container: Container) {
-        container.autoregister(CommentPresenting.self, initializer: CommentPresenter.init)
-
-        container.register(LoginCredentialsStoring.self, name: "LoginCredentials") { _ in
-            return LoginCredentialsStore(userDefaults: UserDefaults.standard)
-        }
-
-        container.register(Deserializer<Comments>.self, name: "Comments") { _ in
-            return Deserializer(CommentsDeserializer(jsonDecoder: JSONDecoder()))
-        }
-
-        container.register(Deserializer<Comment>.self, name: "Comment") { _ in
-            return Deserializer(CommentDeserializer(jsonDecoder: JSONDecoder()))
-        }
 
         container.register(Deserializer<Debate>.self, name: "Debate") { _ in
             return DebateDeserializer.build()
@@ -32,13 +19,6 @@ class APIAssembly: Assembly {
                                URLProvider: resolver ~> URLProviding.self)
         }
 
-        container.register(FetchCommentsServiceProtocol.self) { resolver in
-            return FetchCommentsService(requestExecutor: resolver ~> RequestExecuting.self,
-                                        commentsDeserializer: resolver ~> (Deserializer<Comments>.self,
-                                                                           name: "Comments"),
-                                        URLProvider: resolver ~> URLProviding.self)
-        }
-
         container.register(PusherConfigurationProviding.self) { _ in
             return PusherConfigurationProvider(pusherKeysDictionary: Bundle.main.infoDictionary)
         }
@@ -47,14 +27,6 @@ class APIAssembly: Assembly {
             let configuration = resolver ~> PusherConfigurationProviding.self
             let options = PusherClientOptions(host: .cluster(configuration.clusterKey))
             return Pusher(key: configuration.appKey, options: options)
-        }
-
-        container.register(CommentsWebSocketProtocol.self) { resolver in
-            return CommentsWebSocket(commentDeserializer: resolver ~> (Deserializer<Comment>.self,
-                                                                          name: "Comment"),
-                                     pusher: resolver ~> Pusher.self,
-                                     lastCredentialsStore: resolver ~> (LoginCredentialsStoring.self,
-                                                                        name: "LoginCredentials"))
         }
 
         container.register(FetchDebateServiceProtocol.self) { resolver in
--- Sources/Framework/Dependency Injection/Assembler+Assemblies.swift
+++ Sources/Framework/Dependency Injection/Assembler+Assemblies.swift
@@ -10,7 +10,7 @@ public extension Assembler {
 
     static var defaultAssemblies: [Assembly] {
         return [MainAssembly(), PinEntryAssembly(), AnswerAssembly(),
-                CommentAssembly(), ValidatorAssembly(), APIAssembly(), ServiceHelpersAssembly()]
+                CommentAssembly(), ValidatorAssembly(), APIAssembly(), ServiceHelpersAssembly(), CommentsServicesAssembly()]
     }
 
 }
