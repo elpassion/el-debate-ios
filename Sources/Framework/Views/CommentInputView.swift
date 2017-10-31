ea2ea5e7c0a06be9f73687a5f59f4ff3f4ed7e77
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-10-31 19:54:01 +0100

    Reorganize Views, add updated iTunesArtwork with proper size.

--- Sources/Framework/Views/CommentInputView.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-class CommentInputView: UIView {    
-}
-
-extension CommentInputView {
-}
---
+++ Sources/Framework/Views/CommentsView/CommentInputView.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+class CommentInputView: UIView {
+
+    init() {
+        super.init(frame: .zero)
+    }
+
+    // MARK: - Required initializer
+
+    required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
+
+extension CommentInputView {
+}
