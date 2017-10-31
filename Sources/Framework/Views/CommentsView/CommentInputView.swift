384700059cdfd38be66ca4fca5873ca22a0af338
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-10-31 20:07:31 +0100

    Initial configuration of CommentInputView and its Factory.

--- Sources/Framework/Views/CommentsView/CommentInputView.swift
+++ Sources/Framework/Views/CommentsView/CommentInputView.swift
@@ -2,8 +2,22 @@ import Foundation
 
 class CommentInputView: UIView {
 
+    let sendCommentButton: UIButton = Factory.sendCommentButton()
+    let commentInputTextField: UITextField = Factory.commentInputTextField()
+
     init() {
         super.init(frame: .zero)
+
+        addSubviews()
+        setupAutolayout()
+    }
+
+    private func addSubviews() {
+        addSubview(sendCommentButton)
+        addSubview(commentInputTextField)
+    }
+
+    private func setupAutolayout() {
     }
 
     // MARK: - Required initializer
@@ -12,4 +26,19 @@ class CommentInputView: UIView {
 }
 
 extension CommentInputView {
+
+    struct Factory {
+
+        static func sendCommentButton() -> UIButton {
+            let sendCommentButton = UIButton()
+            return sendCommentButton
+        }
+
+        static func commentInputTextField() -> UITextField {
+            let commentInputTextField = UITextField()
+            return commentInputTextField
+        }
+
+    }
+
 }
