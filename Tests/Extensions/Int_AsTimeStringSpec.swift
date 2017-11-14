0704a9bb2f9e7410a2502e90b657889d0ccd6b0f
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-11-14 19:38:18 +0100

    Fix Unix Time Converting.

--- Tests/Extensions/Int_AsTimeStringSpec.swift
+++ Tests/Extensions/Int_AsTimeStringSpec.swift
@@ -5,10 +5,20 @@ import Quick
 internal class Int_AsTimeStringSpec: QuickSpec {
 
     override func spec() {
+        var testedInteger: NSInteger!
+
+        beforeEach {
+            testedInteger = 1510681264000
+        }
+
+        afterEach {
+            testedInteger = nil
+        }
+
         describe("AsTimeString") {
             it("should convert timestamp from API to proper value") {
-                let testInt = 1510681264000
-                let output = testInt.asTimeString()
+                let output = testedInteger.asTimeString()
+                expect(output) == "18:41"
             }
         }
     }
